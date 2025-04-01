312344ee4709c49b45f9c8feb8f26cd5fce1846e
Mat <mathewdenison@gmail.com>
2025-04-01 17:15:58 -0400

    updates

--- pto_management_service/asgi.py
+++ pto_management_service/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pto_management_service project.
+ASGI config for pto-management-service project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pto_management_service.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pto-management-service.settings')
 
 application = get_asgi_application()
--- pto_management_service/urls.py
+++ pto_management_service/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for pto_management_service project.
+URL configuration for pto-management-service project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
