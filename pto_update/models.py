fe99328e038b4fcf2f605a255708fe1cb096eecf
Mat <mathewdenison@gmail.com>
2025-04-05 19:51:23 -0400

    updates

--- pto_update/models.py
+++ pto_update/models.py
@@ -1,6 +1,9 @@
-from django.apps import AppConfig
+from django.db import models
 
 
-class PtoUpdateConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pto_management_service.pto_update'
+class PTO(models.Model):
+    employee_id = models.IntegerField()  # stores the id of the employee
+    balance = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"PTO balance for employee with id {self.employee_id}: {self.balance} hours"
