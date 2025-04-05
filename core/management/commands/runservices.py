d3f7ff13b2b37e5b42d7ba53fb68cdb8b6725f00
Mat <mathewdenison@gmail.com>
2025-04-05 11:40:03 -0400

    updates

--- core/management/commands/runservices.py
+++ core/management/commands/runservices.py
@@ -1,7 +1,7 @@
-import logging
 from django.core.management import BaseCommand
-from multiprocessing import Process
+from threading import Thread
 from importlib import import_module
+import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -16,15 +16,14 @@ class Command(BaseCommand):
             'user_pto.scripts.process_messages',
         ]
 
-        processes = []
+        threads = []
         for service in services:
             logger.info(f"Launching service: {service}")
             module = import_module(service)
-            process = Process(target=module.run)
-            process.start()
-            logger.info(f"Started {service} with PID {process.pid}")
-            processes.append(process)
+            thread = Thread(target=module.run, daemon=True)
+            thread.start()
+            logger.info(f"Started {service}")
+            threads.append(thread)
 
-        for process in processes:
-            process.join()
-
+        for thread in threads:
+            thread.join()
